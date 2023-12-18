4cbde7486e737e02484dcac79da75fe9aa3890a6
John Hanekom <144270636+john-hanekom-1@users.noreply.github.com>
2023-12-18 19:37:47 +0000

    added main.py for the 1st learning guide

---
+++ Python/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Created by: John Hanekom
+Created on: Dec 2023
+This program prints "Hello, World!" on the PyBadge screen.
+"""
+
+
+def game_scene():
+    print("\n\n\n")  # Blank space
+    print("Hello, World!")
+
+    while True:
+        # Repeat forever
+        pass  # Placeholder
+
+
+if __name__ == "__main__":
+    game_scene()
